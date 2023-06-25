161e40251144e57901d42aa1daea41f640cdc3e2
unknown <eslitish@gmail.com>
2023-06-25 12:05:37 -0500

    Hola

---
+++ Mio.py
@@ -0,0 +1,30 @@
+import os
+import random
+import math
+import pygame
+from os import listdir
+from os.path import isfile, join
+
+pygame.init()
+#Start the game 
+
+pygame.display.set_caption("Rodro Jogo")
+#For the title v>:
+
+BG_color = (250, 250, 250)
+#Why 250250250 would be a white BG color v:???????
+
+Width, Height = 1000 , 800
+FPS = 60
+#Cam i put more than 60 fps? v:
+Player_velocity = 5
+
+window = pygame.display.set_mode((Width, Height))
+
+
+
+
+
+
+
+
